897e01411ad337193770fe7f1b7782293efd7d75
Abhradepp <abhradeep456@gmail.com>
2026-04-14 01:30:18 +0530

    final fix JSON response

--- main.py
+++ main.py
@@ -30,28 +30,34 @@ async def predict(file: UploadFile = File(...)):
     nparr = np.frombuffer(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    results = model(img)
+    results = model(img, conf=0.25)
 
-    #counting of microplastics
-    if results[0].boxes is not None:
-        count = len(results[0].boxes)
-    else:
-        count = 0
+    count = len(results[0].boxes) if results[0].boxes is not None else 0
 
-    #draw boxes on the given image..
-    
     annotated = results[0].plot()
-    
-    #convert to jpeg
     _, buffer = cv2.imencode(".jpg", annotated)
     img_base64 = base64.b64encode(buffer).decode("utf-8")
 
-    #return both
+    return {
+        "count": count,
+        "image": img_base64
+    }
 
-    return JSONResponse({
-        "count" : count,
-        "image" : img_base64
-    })
+from fastapi.responses import Response
+
+@app.post("/predict-image")
+async def predict_image(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    nparr = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    results = model(img, conf=0.25)
+
+    annotated = results[0].plot()
+    _, buffer = cv2.imencode(".jpg", annotated)
+
+    return Response(content=buffer.tobytes(), media_type="image/jpeg")
 
     
 
