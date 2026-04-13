0c377039f1552fe6f5c5e3da4e25d03eeea82826
Abhradepp <abhradeep456@gmail.com>
2026-04-14 01:08:34 +0530

    fix return image + count

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ from fastapi import FastAPI, File, UploadFile
 from ultralytics import YOLO
 import cv2
 import numpy as np
-from fastapi.responses import Response
+from fastapi.responses import JSONResponse
+import base64
 app = FastAPI()
 
 model = YOLO("best.pt")
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
@@ -21,12 +32,26 @@ async def predict(file: UploadFile = File(...)):
 
     results = model(img)
 
+    #counting of microplastics
+    if results[0].boxes is not None:
+        count = len(results[0].boxes)
+    else:
+        count = 0
+
     #draw boxes on the given image..
+    
     annotated = results[0].plot()
+    
     #convert to jpeg
     _, buffer = cv2.imencode(".jpg", annotated)
+    img_base64 = base64.b64encode(buffer).decode("utf-8")
 
-    return Response(content = buffer.tobytes(), media_type = "image/jpeg")
+    #return both
+
+    return JSONResponse({
+        "count" : count,
+        "image" : img_base64
+    })
 
     boxes = results[0].boxes.xyxy.tolist()
     count = len(boxes)
